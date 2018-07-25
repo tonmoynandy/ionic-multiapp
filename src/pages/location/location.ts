b4ee6b02c35389da99e9f7fb475061c197d0efef
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-07-25 17:22:24 +0530

    update geo location

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Diagnostic } from '@ionic-native/diagnostic';
 //import { AndroidPermissions } from '@ionic-native/android-permissions';
@@ -16,37 +16,39 @@ import { Diagnostic } from '@ionic-native/diagnostic';
   templateUrl: 'location.html',
 })
 export class LocationPage {
+	locationData : any = {
+		lat : '',
+		lng : ''
+	}
 	constructor(public navCtrl: NavController, 
-  	public navParams: NavParams,
+		public navParams: NavParams,
+		public platform : Platform,
   	public geoLoc : Geolocation,
   	public alertCtrl : AlertController,
   	public diagnostic: Diagnostic
   	//public permissions : AndroidPermissions
   	) {
+			this.platform.ready().then(()=>{
+				this.geoLoc.getCurrentPosition()
+				.then((resp : any) =>
+				{
+					console.log(resp);
+					console.log('Geolocation mocked inserted!');
+					this.locationData.lat = resp['coords']['latitude'];
+					this.locationData.lng = resp['coords']['longitude'];
+					
 
-  }
-	locationData : any = {
-		lat : '',
-		lng : ''
-	}
-  ionViewDidLoad() {
-  	
-	  this.geoLoc.getCurrentPosition()
-	  .then((resp : any) =>
-	  {
-	     console.log(resp);
-			 console.log('Geolocation mocked inserted!');
-			 this.locationData.lat = resp['coords']['latitude'];
-			 this.locationData.lng = resp['coords']['longitude'];
-			 
-
-	  })
-	  .catch((error : any) =>
-	  {
-	     console.log('Error getting location', error);
-		});
-		
-	console.log('ionViewDidLoad LocationPage');
+				})
+				.catch((error : any) =>
+				{
+					console.log('Error getting location', error);
+				});
+			})
+	
+		}
+	
+  ionViewDidLoad() {	
+		console.log('ionViewDidLoad LocationPage');
   }
 
 }
