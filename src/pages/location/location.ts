582bfe652310ea3e547417186718d7902d667715
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-07-25 18:34:39 +0530

    update geo location

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, Platform } from 'ionic-angular';
+import { Component , ViewChild, ElementRef} from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, Platform, LoadingController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-import { Diagnostic } from '@ionic-native/diagnostic';
-//import { AndroidPermissions } from '@ionic-native/android-permissions';
+
 /**
  * Generated class for the LocationPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+declare var google;
 @IonicPage()
 @Component({
   selector: 'page-location',
   templateUrl: 'location.html',
 })
 export class LocationPage {
+	@ViewChild('map') mapElement: ElementRef;
+  map: any;
 	locationData : any = {
 		lat : '',
 		lng : ''
@@ -24,10 +25,15 @@ export class LocationPage {
 		public navParams: NavParams,
 		public platform : Platform,
   	public geoLoc : Geolocation,
-  	public alertCtrl : AlertController,
-  	public diagnostic: Diagnostic
+		public alertCtrl : AlertController,
+		public loadCtrl : LoadingController
+  	
   	//public permissions : AndroidPermissions
   	) {
+			let loader = this.loadCtrl.create({
+				content : "Map is Loading"
+			})
+			loader.present();
 			this.platform.ready().then(()=>{
 				this.geoLoc.getCurrentPosition()
 				.then((resp : any) =>
@@ -36,8 +42,10 @@ export class LocationPage {
 					console.log('Geolocation mocked inserted!');
 					this.locationData.lat = resp['coords']['latitude'];
 					this.locationData.lng = resp['coords']['longitude'];
-					
+					this.loadMap();
+					loader.dismiss();
 
+					
 				})
 				.catch((error : any) =>
 				{
@@ -49,6 +57,27 @@ export class LocationPage {
 	
   ionViewDidLoad() {	
 		console.log('ionViewDidLoad LocationPage');
+		
   }
-
+	
+	loadMap(){
+ 
+    let latLng = new google.maps.LatLng(this.locationData.lat, this.locationData.lng);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+		this.addMarker(this.locationData);
+	}
+	addMarker(latLng){
+		new google.maps.Marker({
+			map: this.map,
+			animation: google.maps.Animation.DROP,
+			position: latLng
+		});
+	}
 }
