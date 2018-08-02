f02e4e0aa5b7c5c094eb4158951cf1db0a12c1a5
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-08-02 12:21:31 +0530

    Photogallery : start work

--- src/pages/photogallery/photogallery.ts
+++ src/pages/photogallery/photogallery.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,Platform } from 'ionic-angular';
 import { PhotoLibrary } from '@ionic-native/photo-library';
 
 /**
@@ -16,32 +16,41 @@ import { PhotoLibrary } from '@ionic-native/photo-library';
 })
 export class PhotogalleryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private photoLibrary: PhotoLibrary) {
-  }
   photoList : string = '';
-  ionViewDidLoad() {
-    this.photoLibrary.requestAuthorization().then(() => {
-      this.photoLibrary.getLibrary().subscribe({
-        next: library => {
-          this.photoList = JSON.stringify(library);
-          library.forEach(function(libraryItem) {
-            console.log(libraryItem.id);          // ID of the photo
-            console.log(libraryItem.photoURL);    // Cross-platform access to photo
-            console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
-            console.log(libraryItem.fileName);
-            console.log(libraryItem.width);
-            console.log(libraryItem.height);
-            console.log(libraryItem.creationDate);
-            console.log(libraryItem.latitude);
-            console.log(libraryItem.longitude);
-            console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
-          });
-        },
-        error: err => { this.photoList = ('could not get photos'); },
-        complete: () => { console.log('done getting photos'); }
-      });
+  
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private photoLibrary: PhotoLibrary,
+    private platform : Platform,
+  ) {
+    this.platform.ready().then(()=>{
+      this.photoLibrary.requestAuthorization().then(() => {
+        this.photoLibrary.getLibrary().subscribe({
+          next: library => {
+            this.photoList = JSON.stringify(library);
+            library.forEach(function(libraryItem) {
+              console.log(libraryItem.id);          // ID of the photo
+              console.log(libraryItem.photoURL);    // Cross-platform access to photo
+              console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
+              console.log(libraryItem.fileName);
+              console.log(libraryItem.width);
+              console.log(libraryItem.height);
+              console.log(libraryItem.creationDate);
+              console.log(libraryItem.latitude);
+              console.log(libraryItem.longitude);
+              console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+            });
+          },
+          error: err => { this.photoList = ('could not get photos'); },
+          complete: () => { console.log('done getting photos'); }
+        });
+      })
+      .catch(err => this.photoList = ('permissions weren\'t granted'));
     })
-    .catch(err => this.photoList = ('permissions weren\'t granted'));
+  }
+  
+  ionViewDidLoad() {
+    
   }
 
 }
